4c928e6069dea47ad0f67c57892e214f99b9fd6e
Trey Zima <trey.m.zima@gmail.com>
2025-01-20 12:29:23 -0700

    Updated Pokedex and Pokecard to add classNames for styling

--- components/Pokecard.js
+++ components/Pokecard.js
@@ -1,10 +1,10 @@
 function Pokecard({ name, img, type, exp }) {
   return (
-    <div>
-      <h3>{name}</h3>
-      <img src={img} alt={name} />
-      <p>Type: {type}</p>
-      <p>EXP: {exp}</p>
+    <div className="Pokecard">
+      <h3 className="Pokecard-title">{name}</h3>
+      <img className="Pokecard-image" src={img} alt={name} />
+      <p className="Pokecard-type">Type: {type}</p>
+      <p className="Pokecard-exp">EXP: {exp}</p>
     </div>
   );
 }
--- components/Pokedex.js
+++ components/Pokedex.js
@@ -1,18 +1,20 @@
 function Pokedex({ pokemon }) {
   return (
-    <div>
-      {pokemon.map((p) => {
-        const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`;
-        return (
-          <Pokecard
-            key={p.id}
-            name={p.name}
-            img={imgUrl}
-            type={p.type}
-            exp={p.base_experience}
-          />
-        );
-      })}
+    <div className="Pokedex">
+      <div className="Pokedex-cards">
+        {pokemon.map((p) => {
+          const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png`;
+          return (
+            <Pokecard
+              key={p.id}
+              name={p.name}
+              img={imgUrl}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
